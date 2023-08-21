957cbf9d974e57ef66cb8d38df325592a04a21bd
Alexis-Contente <alexis.contente@gmail.com>
2023-08-21 12:16:58 +0200

    Create DB and connection with Prisma

---
+++ app/api/contacts/route.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET(request: NextRequest) {
+  try {
+    const result = await prisma.contact.findMany()
+    return NextResponse.json(result);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.error();
+  }
+}
