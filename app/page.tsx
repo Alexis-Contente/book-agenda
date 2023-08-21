572fe9692b65928200c6f2aea23aa4d2aaa8d9ac
Alexis-Contente <alexis.contente@gmail.com>
2023-08-21 16:24:28 +0200

    Card CSS

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
         Book agenda
       </h1>
 
-      <nav className="w-1/2 mx-auto mt-12 space-x-2 flex">
+      <nav className="w-1/2 mx-auto mt-12 mb-12 space-x-2 flex">
         <input
           type="text"
           placeholder="Search contact"
@@ -18,6 +18,22 @@ export default function Home() {
           Add
         </button>
       </nav>
+
+      <div className="w-1/2 mx-auto block rounded-lg bg-white p-6 flex flex-row mb-4">
+        <div className="w-1/2">
+          <p className="my-2 text-base uppercase">NAME FIRSTNAME</p>
+          <p className="my-2 text-base">EMAIL</p>
+          <p className="my-2 text-base">DATE OF BIRTH</p>
+        </div>
+        <div className="w-1/2 flex flex-col items-end justify-center	gap-1">
+          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-36">
+            MODIFY
+          </button>
+          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded w-36">
+            DELETE
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
