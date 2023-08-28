faa789e1eb47c1fa950df28cf0723d02277c8517
Alexis-Contente <alexis.contente@gmail.com>
2023-08-28 14:35:16 +0200

    axios put done

--- app/api/contact/[id]/route.ts
+++ app/api/contact/[id]/route.ts
@@ -2,6 +2,14 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { PrismaClient } from "@prisma/client";
 
+type Contact = {
+  firstname: string;
+  lastname: string;
+  email: string;
+  birth: string;
+  information: string;
+}
+
 const prisma = new PrismaClient();
 
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
@@ -37,3 +45,27 @@ export async function DELETE(request: NextRequest, { params }: { params: { id: n
     return NextResponse.error();
   }
 }
+
+export async function PUT(request: NextRequest, { params }: { params: { id: number } }) {
+  const {id} = params;
+  console.log("passla");
+  try {
+    const requestBody = await request.json();
+    console.log(requestBody);
+    const { firstname, lastname, email, birth, information }: Contact = requestBody;
+    
+    // if (!firstname || !lastname || !email || !birth || !information) {
+    //   return NextResponse.json({ error: 'Tous les champs sont requis.' }, { status: 400 });
+    // }
+
+    const result = await prisma.contact.update({
+      where: {id: Number(id)},
+      data: { firstname, lastname, email, birth, information },
+    });
+
+    return NextResponse.json(result);
+  }
+  catch (error) {
+    console.error("Erreur lors de la création de l'objet contact",error);
+    return NextResponse.error();
+  }}  
--- app/page.tsx
+++ app/page.tsx
@@ -26,8 +26,23 @@ export default function Home() {
     setShowModal(!showModal);
   };
 
+  // TOGGLE MODAL EDIT
+  const [showModalEdit, setShowModalEdit] = useState(false);
+  const openModalEdit = () => {
+    setShowModalEdit(!showModalEdit);
+  };
+
   // FUNCTION THAT GET INFORMATIONS OF CONTACTS FOR DISPLAY
   const [contacts, setContacts] = useState<Contacts[]>([]);
+
+  // FUNCTION THAT GET INFORMATIONS OF CONTACTS FOR EDIT
+  const [selectedContact, setSelectedContact] = useState<Contacts | null>(null);
+
+  // TOGGLE EDIT MODAL
+  const handleEdit = (contact: Contacts) => {
+    setSelectedContact(contact);
+    setShowModalEdit(true);
+  };
 
   // REQUEST TO GET DATA CONTACT
   const contactsData = async () => {
@@ -95,6 +110,32 @@ export default function Home() {
     }
   };
 
+  // HANDLE THAT EDIT A CONTACT
+  const [firstname, setFirstname] = useState(selectedContact?.firstname);
+  const [lastname, setLastname] = useState(selectedContact?.lastname);
+  const [email, setEmail] = useState(selectedContact?.email);
+  const [birth, setBirth] = useState(selectedContact?.birth);
+  const [information, setInformation] = useState(selectedContact?.information);
+
+  const handleSubmitEdit = async (id: string) => {
+    try {
+      if (contacts) {
+        await axios.put(`/api/contact/${id}`, {
+          firstname,
+          lastname,
+          email,
+          birth,
+          information,
+        });
+        console.log("Contact updated");
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      window.location.reload();
+    }
+  };
+
   return (
     <main className="min-h-screen">
       <h1 className="pt-12 text-center text-4xl font-bold uppercase ">
@@ -137,7 +178,10 @@ export default function Home() {
               <p className="my-2 text-base">{contact.information}</p>
             </div>
             <div className="w-1/2 flex flex-col items-end justify-center	gap-1">
-              <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-36 flex justify-between items-center">
+              <button
+                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-36 flex justify-between items-center"
+                onClick={() => handleEdit(contact)}
+              >
                 <Image alt="Image d'un icône d'edition" src={editIcon}></Image>
                 <p className="uppercase">Edit</p>
               </button>
@@ -272,6 +316,148 @@ export default function Home() {
                       className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-dark"
                       placeholder="Other informations"
                       required
+                    ></textarea>
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Save
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/* MODAL EDITFORM */}
+
+      {/* <!-- Main modal --> */}
+      {showModalEdit && selectedContact && (
+        <div
+          id="authentication-modal"
+          aria-hidden="true"
+          className="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full flex items-center justify-center"
+        >
+          <div className="absolute w-full max-w-md max-h-full">
+            {/* <!-- Modal content --> */}
+            <div className="relative bg-white rounded-lg shadow dark:bg-gray-400">
+              <button
+                type="button"
+                className="absolute top-3 right-2.5 text-red-600 bg-transparent hover:bg-red-200 hover:text-red-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-red-600 dark:hover:text-white"
+                data-modal-hide="authentication-modal"
+                onClick={openModalEdit}
+              >
+                <svg
+                  className="w-3 h-3"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                  />
+                </svg>
+                <span className="sr-only">Close modal</span>
+              </button>
+              <div className="px-6 py-6 lg:px-8">
+                <form
+                  className="space-y-6"
+                  action="#"
+                  ref={formRef}
+                  onSubmit={() => handleSubmitEdit(selectedContact.id)}
+                >
+                  <div>
+                    <label
+                      htmlFor="lastname"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Lastname
+                    </label>
+                    <input
+                      type="text"
+                      name="lastname"
+                      id="lastname"
+                      className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-dark"
+                      placeholder="Enter lastname"
+                      defaultValue={selectedContact.lastname}
+                      onChange={(e) => setLastname(e.target.value)}
+                      required
+                    ></input>
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="firstname"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Firstname
+                    </label>
+                    <input
+                      type="text"
+                      name="firstname"
+                      id="firstname"
+                      className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-dark"
+                      placeholder="Enter firstname"
+                      defaultValue={selectedContact.firstname}
+                      onChange={(e) => setFirstname(e.target.value)}
+                      required
+                    ></input>
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-dark"
+                      placeholder="Enter email"
+                      defaultValue={selectedContact.email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                    ></input>
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Date of birth
+                    </label>
+                    <input
+                      type="text"
+                      name="birth"
+                      id="birth"
+                      className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-dark"
+                      placeholder="Enter date of birth"
+                      defaultValue={selectedContact.birth}
+                      onChange={(e) => setBirth(e.target.value)}
+                      required
+                    ></input>
+                    <label
+                      htmlFor="email"
+                      className="block mb-2 mt-4 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Information
+                    </label>
+                    <textarea
+                      name="information"
+                      id="information"
+                      className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-dark"
+                      placeholder="Other informations"
+                      defaultValue={selectedContact.information}
+                      onChange={(e) => setInformation(e.target.value)}
                     ></textarea>
                   </div>
 
